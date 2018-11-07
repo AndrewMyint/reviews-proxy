01341a4e83493b1da2caafa69a8259185962ddc9
michaelcodingstuff <33077654+michaelcodingstuff@users.noreply.github.com>
2018-11-07 07:36:58 -0800

    proxy working

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
-
+const proxy = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const compression = require('compression');
 const fetch = require('node-fetch');
+
 const app = express();
 const port = 3010;
+
+app.use(compression());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.resolve(__dirname, '../client/')));
+app.get('/', (req, res) => {
+  res.redirect('/movies/1');
+});
+
+app.use(express.static('../public'));
+app.get('/movies/:id', (req, res) => {
+  const filePath = path.join(__dirname, '../public/index.html');
+  res.sendFile(filePath);
+});
+
+// app.use(express.static(path.resolve(__dirname, '../public/')));
 
 app.use((request, response, next) => {
   response.set({
@@ -19,76 +32,54 @@ app.use((request, response, next) => {
   next();
 });
 
-// dada
-app.get('/api/movies/:genre/relatedmovies', (req, res) => {
-  fetch(`http://localhost:3003/api/movies/${req.params.genre}/relatedmovies`)
-    .then(response => {
-      response.json()
-        .then(data => {
-          res.send(data);
-        });
-    })
-    .catch(error => {
-      res.send(500, error);
-    });
-});
+// related
+app.use(
+  '/api/movies/:genre/relatedmovies',
+  proxy({
+    target: 'http://localhost:3003',
+    changeOrigin: true
+  })
+);
 
 // reviews
-app.get('/api/movies/:movieid/rating', (req, res) => {
-  fetch(`http://localhost:3013/api/movies/${req.params.movieid}/rating`)
-    .then(response => {
-      response.json()
-        .then(data => {
-          res.send(data);
-        });
-    })
-    .catch(error => {
-      res.send(500, error);
-    });
-});
 
-app.get('/api/movies/:movieid/reviews', (req, res) => {
-  fetch(`http://localhost:3013/api/movies/${req.params.movieid}/reviews`)
-    .then(response => {
-      response.json()
-        .then(data => {
-          res.send(data);
-        });
-    })
-    .catch(error => {
-      res.send(500, error);
-    });
-});
+app.use(
+  '/api/movies/:movieid/rating',
+  proxy({
+    target: 'http://localhost:3013',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/api/movies/:movieid/reviews',
+  proxy({
+    target: 'http://localhost:3013',
+    changeOrigin: true
+  })
+);
 
 // summary
-app.get('/api/movies/:movieId/summary', (req, res) => {
-  fetch(`http://localhost:3007/api/movies/${req.params.movieId}/summary`)
-    .then(response => {
-      response.json()
-        .then(data => {
-          res.send(data);
-        })
-    })
-    .catch(error => {
-      res.send(500, error);
-    });
-});
+
+app.use(
+  '/api/movies/:movieId/summary',
+  proxy({
+    target: 'http://localhost:3007',
+    changeOrigin: true
+  })
+);
 
 // movie times
-app.get('/api/moviesbyid/:movieid/:date/:location', (req, res) => {
-  const { movieid, date, location } = req.params;
-  fetch(`http://localhost:3002/api/moviesbyid/${movieid}/${date}/${location}`)
-    .then(response => {
-      response.json()
-        .then(data => {
-          res.send(data);
-        });
-    })
-    .catch(error => {
-      res.send(500, error);
-    });
-});
 
+app.use(
+  '/api/moviesbyid/:movieid/:date/:location',
+  proxy({
+    target: 'http://localhost:3002',
+    changeOrigin: true
+  })
+);
+
+// not being used
 // app.get('/api/movies/:movie/:date/:location', (req, res) => {
 //   const { movie, date, location } = req.params;
 //   fetch(`http://localhost:3002/api/movies/${movie}/${date}/${location}`)
